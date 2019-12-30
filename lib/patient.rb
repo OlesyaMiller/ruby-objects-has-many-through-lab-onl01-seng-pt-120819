bdcb53be18d6f4f371a4a3445228976e54a8947d
Olesya Miller <olesyam.miller@gmail.com>
2019-12-30 19:12:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,7 +19,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.collect do |appointment|
+    Appointment.all.select do |appointment|
       appointment.patient == self 
     end
   end
