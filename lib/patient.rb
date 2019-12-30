cb6724266c08b5bf32b4013346b63ee8f2d4002c
Olesya Miller <olesyam.miller@gmail.com>
2019-12-30 19:15:27 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,37 @@
+class Patient 
+  
+  @@all = []
+  
+  attr_reader :name 
+  attr_writer
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self 
+    end
+  end
+  
+  def doctors
+    self.appointments.collect do |appointment|
+      appointment.doctor 
+    end
+  end
+end
+
+
+
+
+
